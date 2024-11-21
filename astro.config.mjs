0d234c6fe6e48fffe41c8fd660d61ad07d095d0c
Michelle Tan <dev.michelletan@gmail.com>
2024-11-21 16:41:27 +0700

    add deployment script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://mctn6.github.io',
+  base: 'astro-petcare',
   integrations: [tailwind()]
 });
